dc8a44ac8fc4a8350e923fe3bb7f868cdc65ea18
huonchan <68013384+huonchan@users.noreply.github.com>
2024-10-29 16:14:10 +0900

    一時保存

--- js/changer.js
+++ js/changer.js
@@ -9,9 +9,9 @@ var convert_list = {
         "text": ["輝け(ローマ字読み)"],
         "frozen": true
     },
-    "殺す":
+    "殺":
     {
-        "text": ["杀殳す"],
+        "text": ["杀殳", "Kill"],
         "frozen": true
     },
     "殺害":
@@ -21,7 +21,7 @@ var convert_list = {
     },
     "障害者":
     {
-        "text": ["SHOW GUY SHA","PerfectHuman(24時間TV)"],
+        "text": ["SHOW GUY SHA", "PerfectHuman(24時間TV)"],
         "frozen": true
     },
     "障害":
@@ -46,7 +46,7 @@ var convert_list = {
     },
     "統合失調症":
     {
-        "text": ["糖質","糖度が高い"],
+        "text": ["糖質", "糖度が高い"],
         "frozen": true
     },
     "キチガイ":
@@ -69,133 +69,638 @@ var convert_list = {
     "話す":
     {
         "text": ["玉音放送"],
-        "low": true
+        "lou": true
     },
     "クソゲー":
     {
         "text": ["ゲー無"],
-        "low": true ,
+        "lou": true,
     },
 
 
     //悪芋変換陽
+    "私の":
+    {
+        "text": ["マイ"],
+        "lou": true,
+    },
     "います":
     {
         "text": ["いるんだぜ"],
-        "low": true
+        "lou": true
     },
     "否":
     {
         "text": ["ノー"],
-        "low": true
+        "lou": true
     },
     "弟":
     {
         "text": ["リトルブラザー"],
-        "low": true
+        "lou": true
     },
     "名前":
     {
         "text": ["ネーム"],
-        "low": true
+        "lou": true
     },
     "一":
     {
         "text": ["ワン"],
-        "low": true
+        "lou": true
     },
     "違い":
     {
         "text": ["ディファレント"],
-        "low": true
+        "lou": true
     },
     "たが":
     {
         "text": ["たBut"],
-        "low": true
+        "lou": true
     },
     "私":
     {
-        "text": ["マイセルフ","マイ"],
-        "low": true
+        "text": ["マイセルフ", "マイ"],
+        "lou": true
     },
-    "芋":
+    "いも":
     {
         "text": ["ポテト"],
-        "low": true
+        "lou": true
     },
     "川":
     {
         "text": ["リバー"],
-        "low": true
+        "lou": true
     },
     "河":
     {
         "text": ["リバー"],
-        "low": true
+        "lou": true
     },
     "あっ":
     {
         "text": ["Aっ"],
-        "low": true
+        "lou": true
     },
     "日":
     {
         "text": ["DAY"],
-        "low": true
+        "lou": true
     },
     "自殺":
     {
         "text": ["殺害"],
-        "low": true ,
+        "lou": true,
         "frozen": false
+    },
+    "中には":
+    {
+        "text": ["インサイドには"],
+        "lou": true,
     },
     "中":
     {
-        "text": ["インサイト"],
-        "low": true ,
+        "text": ["イン"],
+        "lou": true,
     },
     "いつも":
     {
         "text": ["Yesつも"],
-        "low": true ,
+        "lou": true,
     },
     //ここよりオリジナル
     "自我":
     {
         "text": ["エゴ"],
-        "low": true ,
+        "lou": true,
     },
     "戦う":
     {
         "text": ["ファイト"],
-        "low": true ,
+        "lou": true,
+    },
+    "男性":
+    {
+        "text": ["メン"],
+        "lou": true
+    },
+    "角":
+    {
+        "text": ["ホーン"],
+        "lou": true
+    },
+    "男":
+    {
+        "text": ["マン"],
+        "lou": true
+    },
+    "性":
+    {
+        "text": ["Sex"],
+        "lou": true
+    },
+    "差別":
+    {
+        "text": ["ヘイト"],
+        "lou": true
+    },
+    "ついた":
+    {
+        "text": ["アタッチ"],
+        "lou": true
+    },
+    "火":
+    {
+        "text": ["ファイア"],
+        "lou": true
+    },
+    "線":
+    {
+        "text": ["ライン"],
+        "lou": true
+    },
+    "絶対":
+    {
+        "text": ["アブソリュート"],
+        "lou": true
+    },
+    "許":
+    {
+        "text": ["forfive"],
+        "lou": true
+    },
+    "ねぇ":
+    {
+        "text": ["no","not exist"],
+        "lou": true
+    },
+    "名":
+    {
+        "text": ["ネーム"],
+        "lou": true
+    },
+    "牛":
+    {
+        "text": ["ビーフ"],
+        "lou": true
+    },
+    "潰して":
+    {
+        "text": ["プレス"],
+        "lou": true
+    },
+    "今":
+    {
+        "text": ["ナウ","now"],
+        "lou": true
+    },
+    "個人":
+    {
+        "text": ["パーソナル"],
+        "lou": true
+    },
+    "人":
+    {
+        "text": ["ピーブル","People"],
+        "lou": true
+    },
+    "店":
+    {
+        "text": ["ショップ"],
+        "lou": true
+    },
+    "企業":
+    {
+        "text": ["カンパニー"],
+        "lou": true
+    },
+    "戦っ":
+    {
+        "text": ["ファイトし"],
+        "lou": true
+    },
+    "戦い":
+    {
+        "text": ["デュエル"],
+        "lou": true
+    },
+    "全":
+    {
+        "text": ["フル"],
+        "lou": true
+    },
+    "身":
+    {
+        "text": ["ボディ"],
+        "lou": true
+    },
+    "霊":
+    {
+        "text": ["ゴースト", "スピリット"],
+        "lou": true
+    },
+    "挑んで":
+    {
+        "text": ["チャレンジして"],
+        "lou": true
+    },
+    "来たよ":
+    {
+        "text": ["comeよ"],
+        "lou": true
+    },
+    "きたよ":
+    {
+        "text": ["comeよ"],
+        "lou": true
+    },
+    "やる":
+    {
+        "text": ["トライ"],
+        "lou": true
+    },
+    "闘志":
+    {
+        "text": ["テンション"],
+        "lou": true
+    },
+    "法":
+    {
+        "text": ["Law"],
+        "lou": true
+    },
+    "内":
+    {
+        "text": ["イン"],
+        "lou": true
+    },
+    "使":
+    {
+        "text": ["ユーズ"],
+        "lou": true
+    },
+    "手":
+    {
+        "text": ["ハンド"],
+        "lou": true
+    },
+    "やる":
+    {
+        "text": ["プレイ"],
+        "lou": true
+    },
+    "異世界":
+    {
+        "text": ["パラレルワールド"],
+        "lou": true
+    },
+    "世界":
+    {
+        "text": ["ワールド"],
+        "lou": true
+    },
+    "異":
+    {
+        "text": ["ディファレンツ"],
+        "lou": true
+    },
+    "さよなら":
+    {
+        "text": ["グッバイ","Good Bye"],
+        "lou": true
+    },
+    "出来事":
+    {
+        "text": ["イベント","Event"],
+        "lou": true
+    },
+    "凄い":
+    {
+        "text": ["グレート","ファニー","Great","Funny"],
+        "lou": true
+    },
+    "能力":
+    {
+        "text": ["アビリティ","スキル"],
+        "lou": true
+    },
+    "得":
+    {
+        "text": ["ゲット","Get"],
+        "lou": true
+    },
+    "いい":
+    {
+        "text": ["Good","グッド"],
+        "lou": true
+    },
+    "マンガ":
+    {
+        "text": ["コミック"],
+        "lou": true
+    },
+    "漫画":
+    {
+        "text": ["コミック"],
+        "lou": true
+    },
+    "現実":
+    {
+        "text": ["リアル","Real"],
+        "lou": true
+    },
+    "動かす":
+    {
+        "text": ["ムービング","Moveing"],
+        "lou": true
+    },
+    "動":
+    {
+        "text": ["ムーブ","Move"],
+        "lou": true
+    },
+    "サヨナラ":
+    {
+        "text": ["グッバイ","Good Bye"],
+        "lou": true
+    },
+    "今すぐ":
+    {
+        "text": ["ハリーアップ"],
+        "lou": true
+    },
+    "今":
+    {
+        "text": ["ナウ","Now"],
+        "lou": true
+    },
+    "心":
+    {
+        "text": ["マインド","Mind"]
+    },
+    "命":
+    {
+        "text": ["ライフ","Life"],
+        "lou": true
+    },
+    "できる":
+    {
+        "text": ["トライ","Try"],
+        "lou": true
+    },
+    "出来る":
+    {
+        "text": ["トライ","Try"],
+        "lou": true
+    },
+    "ないぞ":
+    {
+        "text": ["None","ノー"],
+        "lou": true
+    },
+    "ない":
+    {
+        "text": ["None","ノー"],
+        "lou": true
+    },
+    "危":
+    {
+        "text": ["Danger","デンジャー"],
+        "lou": true
+    },
+    "会":
+    {
+        "text": ["ミーツ","meat"],
+        "lou": true
+    },
+    "ラック":
+    {
+        "text": ["Luck"],
+        "lou": true
+    },
+    "近":
+    {
+        "text": ["near"],
+        "lou": true
+    },
+    "跳ね":
+    {
+        "text": ["ホップ","Hop"],
+        "lou": true
+    },
+
+    //日本語変換
+    "現世"://todo:分類審議中
+    {
+        "text": ["現し世"],
+        "japanese": true
+    },
+    "レディース":
+    {
+        "text": ["女達の"],
+        "japanese": true
+    },
+    "レディー":
+    {
+        "text": ["女"],
+        "japanese": true
+    },
+    "デー":
+    {
+        "text": ["日"],
+        "japanese": true
+    },
+    "デイ":
+    {
+        "text": ["日"],
+        "japanese": true
+    },
+    "リベンジ":
+    {
+        "text": ["成り上がり"],
+        "japanese": true
     },
 };
 
+function karitxt_otouto_killer() {
+
+    const ta = document.getElementById("origin_text_area");
+    var t =
+        `当職の弟を当職が殺したなどという当職のアイデンティティを否定する投稿が多数なされておりました。
+当職の弟は厚史という名前でした。
+一つ違いの弟でした。喧嘩もしましたが、私にとってのかけがいのない弟でした。
+弟は、地元の悪いもの達に、恐喝され、多摩川の河川敷で、集団暴行にあった翌日に親にもいえず自殺しました。
+弟が16、私が17のことでした。私の中にはいつも弟がいます。`
+        ;
+    ta.value = t;
+
+    const ota = document.getElementById("origin_text_count");
+    ota.innerText = `表現の自由　文字数:${t.length}`
+}
+
+function karitxt_gyukaku_misojini() {
+
+    const ta = document.getElementById("origin_text_area");
+    var t =
+        `牛角の男性差別商法
+私の導火線に火がついた
+
+絶対許さねえぞ
+
+今まで個人店のレディースデーは数えきれないほど戦って潰してしてきたけど
+今度はそれなりに名の通った企業相手だから全身全霊で戦いを挑んでやる
+
+久々に闘志が湧いてきたよ
+
+合法の範囲内で使える手はどんな手を使ってでも戦ってやる`;
+    ta.value = t;
+
+    const ota = document.getElementById("origin_text_count");
+    ota.innerText = `表現の自由　文字数:${t.length}`
+}
+
+
+function karitxt_orekayo_sinegomi()
+{
+    
+    const ta = document.getElementById("origin_text_area");
+    var t =
+        `最近の異世界の男はトラックに跳ねられるなりで現世からサヨナラする出来事あれば、
+異世界でなんか凄い能力を得て異世界でリベンジできるらしいから、
+チャレンジすればいいんじゃないかな
+そのマンガが事実なら現実で心臓動かしてる場合じゃないぞ
+今すぐ絶命の危機に会え！`;
+    ta.value = t;
+
+    const ota = document.getElementById("origin_text_count");
+    ota.innerText = `表現の自由　文字数:${t.length}`
+}
+
+function karitxt_sorairo()
+{
+    
+    const ta = document.getElementById("origin_text_area");
+    var t =
+        `どのような親に
+
+どのような家族環境で育ち 
+
+そのような行為いつからして 
+
+学校ではどんな生徒で 
+
+学校では何をみて 
+
+空の色は何色だったか 
+
+君は泣いたことがあるか 
+
+何があったのか 
+
+親の愛を感じたことはあるか 
+
+私に殺害予告をしたときの気持ちは何で 
+
+それは君の気持ちにどのような快感を与え 
+
+ある日来た警察に対して君は真っ正面を向くことができたのか
+
+そのときの親の顔を見たか 
+
+君は親の悲しみを知っているのか 
+
+1人淋しく過ごした夜に何を感じ 
+
+守ってくれた弁護士の説諭に何を感じ 
+
+君はいま何を思うのか 
+
+ある日来た警察に対して君は真っ正面を向くことができたのか
+
+そのときの親の顔を見たか
+
+君は親の悲しみを知っているのか
+
+1人淋しく過ごした夜に何を感じ
+
+守ってくれた弁護士の説諭に何を感じ
+
+君はいま何を思うのか
+
+君はいま親の気持ちがわかるか
+
+君は人を傷付ける人間なのか
+
+君は親を殺すことができるのか
+
+人は人を傷付けて幸せになれるのか
+
+いま君は何を見ているのか
+
+空は何色か
+
+人は人を愛さなければない
+
+君は親の愛に気が付かなくてはならない
+
+いまが君の人生の始まりだ
+
+君はこれからどのような大人になるのか
+
+俺は君に人を傷付けるのではなく人を助ける人間になってほしい
+
+俺は君の20年後を見ている
+`;
+    ta.value = t;
+
+    const ota = document.getElementById("origin_text_count");
+    ota.innerText = `表現の自由　文字数:${t.length}`
+}
 
 function initialize() {
     //仮
-
-    const ta = document.getElementById("origin_text_area");
-    ta.value =
-        `当職の弟を当職が殺したなどという当職のアイデンティティを否定する投稿が多数なされておりました。
-当職の弟は厚史という名前でした。一つ違いの弟でした。喧嘩もしましたが、私にとってのかけがいのない弟でした。
-弟は、地元の悪いもの達に、恐喝され、多摩川の河川敷で、集団暴行にあった翌日に親にもいえず自殺しました。
-弟が16、私が17のことでした。
-私の中にはいつも弟がいます。`;
 
 }
 
 function changeText() {
     var ta = document.getElementById("origin_text_area");
     var text = ta.value;
+
+    const lue_value = document.getElementById("lou_change").valueAsNumber;
+    const japanese_value = document.getElementById("japanese_change").valueAsNumber;
+
     for (const key in convert_list) {
-        //const randomIndex = Math.floor(Math.random() * convert_list[key].length);
-        const regex = new RegExp(key, 'g'); // 変数 apple を使って正規表現を作成
-        text = text.replace(regex, convert_list[key]["text"][0]);
-        //document.getElementById("change_text_area").value = convert_list[key]["text"][0];
+
+        if (convert_list[key].hasOwnProperty("frozen") &&
+            convert_list[key]["frozen"])
+        {
+            const randomIndex = Math.floor(Math.random() * convert_list[key]["text"].length);
+            const regex = new RegExp(key, 'g'); // 変数 apple を使って正規表現を作成
+            text = text.replace(regex, convert_list[key]["text"][randomIndex]);
+        }
+
+        if (convert_list[key].hasOwnProperty("lou") &&
+            convert_list[key]["lou"] &&
+            Math.floor(Math.random() * 100) <= lue_value)//ルー判定
+        {
+            const randomIndex = Math.floor(Math.random() * convert_list[key]["text"].length);
+            const regex = new RegExp(key, 'g'); // 変数 apple を使って正規表現を作成
+            text = text.replace(regex, convert_list[key]["text"][randomIndex]);
+        }
+
+        if (convert_list[key].hasOwnProperty("japanese") &&
+            convert_list[key]["japanese"] &&
+            Math.floor(Math.random() * 100) <= japanese_value)//ルー判定
+        {
+            const randomIndex = Math.floor(Math.random() * convert_list[key]["text"].length);
+            const regex = new RegExp(key, 'g'); // 変数 apple を使って正規表現を作成
+            text = text.replace(regex, convert_list[key]["text"][randomIndex]);
+        }
     }
+    const ota = document.getElementById("change_text_count");
+    ota.innerText = `表現の不自由　文字数:${text.length}`
     document.getElementById("change_text_area").value = text;
 }
 
